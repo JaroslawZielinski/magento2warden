af1f5f790a7646ccc994b26448f69e01b28cfcaf
JaroslawZielinski <jarek.zielinski82@gmail.com>
2021-11-04 10:25:00 +0100

    Update magento-setup.sh

--- includes/magento-setup.sh
+++ includes/magento-setup.sh
@@ -12,13 +12,13 @@ OTPAUTH_URL=$(printf "otpauth://totp/%s%%3Alocaladmin%%40example.com?issuer=%s&s
 OATHTOOL=$(warden env exec -T php-fpm bash -c "oathtool -s 30 -w 10 --totp --base32 \"${TFA_SECRET}\"")
 warden env exec -T php-fpm bash -c "
 ### Generate localadmin user
-#bin/magento admin:user:create \
-#    --admin-password=\"${ADMIN_PASS}\" \
-#    --admin-user=\"${ADMIN_USER}\" \
-#    --admin-firstname=\"Local\" \
-#    --admin-lastname=\"Admin\" \
-#    --admin-email=\"${ADMIN_USER}@example.com\"
-#printf \"u: %s\np: %s\n\" \"${ADMIN_USER}\" \"${ADMIN_PASS}\"
+bin/magento admin:user:create \
+    --admin-password=\"${ADMIN_PASS}\" \
+    --admin-user=\"${ADMIN_USER}\" \
+    --admin-firstname=\"Local\" \
+    --admin-lastname=\"Admin\" \
+    --admin-email=\"${ADMIN_USER}@example.com\"
+printf \"u: %s\np: %s\n\" \"${ADMIN_USER}\" \"${ADMIN_PASS}\"
 
 bin/magento config:set --lock-env twofactorauth/general/force_providers google
 bin/magento security:tfa:google:set-secret \"${ADMIN_USER}\" \"${TFA_SECRET}\"
