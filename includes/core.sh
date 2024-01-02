3d2aee551e54276907aa644aaf3c915993506a52
Jarosław Zieliński <jaroslaw.zielinski@polcode.net>
2024-01-02 12:37:06 +0100

    Fixes 01

--- includes/core.sh
+++ includes/core.sh
@@ -3,4 +3,8 @@
 function ::() {
     printf "\n %s : %s\n=======================================\n" "$(date '+%Y-%m-%d %H:%M:%S')" "$1"
 }
-. .env
+# @see https://stackoverflow.com/a/66118031/339144
+set -a
+source <(cat .env | \
+    sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g")
+set +a
