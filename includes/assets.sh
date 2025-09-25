b8081e094c93cf70079068af9963c2e394c6b2a3
Jerry <jerry@livingproofcreative.com>
2025-09-25 14:31:25 +0200

    Front work fixes

--- includes/assets.sh
+++ includes/assets.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 source includes/core.sh
 :: "Start"
-warden env exec -T php-fpm bin/magento setup:static-content:deploy -f en_US
-warden env exec -T php-fpm bin/magento cache:clean
-warden env exec -T php-fpm bin/magento cache:flush
+warden env exec -T php-fpm bin/magento setup:static-content:deploy -f en_US # pl_PL
 :: "Finished."
